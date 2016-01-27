330d93d038d7f6829e8f7fe099925b8019eaceb4
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-01-27 11:22:08 -0800

    feat: added App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
 ReactDOM.render(
-  <div>Hello!</div>,
+  <App />,
   document.getElementById('app')
 );
