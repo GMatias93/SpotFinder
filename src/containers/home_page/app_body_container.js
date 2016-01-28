9e9713529fbc8db66bfb0e9e708712f01afa9d2c
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-01-28 15:13:55 -0800

    feat: added basic suggested users feature, need to fix css

--- src/containers/home_page/app_body_container.js
+++ src/containers/home_page/app_body_container.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NewsFeed from './news_feed/news_feed_container';
+import SuggestedUsers from './popular_users/suggested_users_container';
 
 export default class AppBodyContainer extends Component {
   render() {
     return (
       <div className="container-fluid">
         <NewsFeed />
+        <SuggestedUsers />
       </div>
     );
   }
---
+++ src/containers/home_page/popular_users/suggested_users_container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class SuggestedUsers extends Component {
+  render() {
+    return (
+      <div className="col-md-3">
+        <ul>
+          <li>SuggestedUsers</li>
+          <li>SuggestedUsers</li>
+          <li>SuggestedUsers</li>
+        </ul>
+      </div>
+
+    );
+  }
+}
