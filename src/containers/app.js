063205df3482424721386a7ee927a677a907812e
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-02 13:48:52 -0800

    fix: removed all  components previously created.

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import NavBar from '../components/nav_bar_component';
-import AppBodyContainer from './home_page/app_body_container';
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <NavBar />
-        <AppBodyContainer />
       </div>
     );
   }
